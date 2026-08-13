834fea52d8e24af7cc0f2c4d4dee3aeca2278561
Sondre Bjerve <sondre00bjerve@gmail.com>
2026-08-13 16:51:22 +0200

    Spillvindu og karakter er laget

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,19 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+    //Konstruktor for Spillvindu
+	sf::RenderWindow window( sf::VideoMode( { 1500, 800 } ), "Lets Build Some Shit!");
+
+	//SpillKarakter (objekt)
+	sf::Texture texture("C:/Users/sondr/OneDrive/Desktop/Git_Projects/Game_1/Assets/PNG/sun.png");
+	sf::Sprite sprite(texture);
+	sprite.setScale({0.06f,0.06f});
+	
+
+	//Spilltekst
+	sf::Font font("C:/Users/sondr/OneDrive/Desktop/Git_Projects/Game_1/Assets/FONT/Doug-Regular.ttf.otf");
+	sf::Text text(font);
+
 
 	while ( window.isOpen() )
 	{
@@ -13,9 +23,25 @@ int main()
 			if ( event->is<sf::Event::Closed>() )
 				window.close();
 		}
+         
+	    //Rydder vinduet fra forrige frame
+		window.clear(sf::Color::Black);
 
-		window.clear();
-		window.draw( shape );
+
+
+        //tegner på neste frame
+ 		window.draw( sprite );
+
+
+		text.setString("Kjøøøøh!");
+		text.setCharacterSize(50);
+		text.setFillColor(sf::Color::Red);
+		text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+
+
+
+		//Displayer nytt frame
 		window.display();
 	}
 }
