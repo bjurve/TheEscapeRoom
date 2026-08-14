ed8877fcc9fe54265b2291959b870f1cc99aaa33
Sondre Bjerve <sondre00bjerve@gmail.com>
2026-08-14 13:40:55 +0200

    la til sirkel + bevegelse

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,68 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
 
 int main()
 {
     //Konstruktor for Spillvindu
-	sf::RenderWindow window( sf::VideoMode( { 1500, 800 } ), "Lets Build Some Shit!");
+	unsigned int width = 1500;
+	unsigned int height = 800;	
+	sf::RenderWindow window( sf::VideoMode( sf::Vector2u(width, height)), "Lets Build Some Shit!");
 
-	//SpillKarakter (objekt)
+	//Objekt (Sprite)
 	sf::Texture texture("C:/Users/sondr/OneDrive/Desktop/Git_Projects/Game_1/Assets/PNG/sun.png");
 	sf::Sprite sprite(texture);
+	sprite.setPosition({800.f,400.f});
 	sprite.setScale({0.06f,0.06f});
+
+	//Objekt (Sirkel)
+	sf::CircleShape circle(50.f);
+	circle.setOrigin(circle.getGeometricCenter());
+	circle.setPosition({width / 4.0f, height / 2.0f} );
+	
+
+
+	
 	
 
 	//Spilltekst
 	sf::Font font("C:/Users/sondr/OneDrive/Desktop/Git_Projects/Game_1/Assets/FONT/Doug-Regular.ttf.otf");
 	sf::Text text(font);
+	text.setPosition({10.f,50.f});
 
-
+	//Game Loop
 	while ( window.isOpen() )
 	{
-		while ( const std::optional event = window.pollEvent() )
+		//Lukker Vindu om bruker trykker X eller Escape
+		while(std::optional event = window.pollEvent())
 		{
-			if ( event->is<sf::Event::Closed>() )
+			if(event->is<sf::Event::Closed>()){
 				window.close();
+			}
+			else if(const auto* keypressed = event->getIf<sf::Event::KeyPressed>())
+			{
+				if(keypressed->scancode == sf::Keyboard::Scancode::Escape){
+					window.close();
+				}
+			}
+
 		}
-         
-	    //Rydder vinduet fra forrige frame
+
+		
+
+
+
+
+	    //Rydder og fjerner forrige frame
 		window.clear(sf::Color::Black);
 
 
 
         //tegner på neste frame
- 		window.draw( sprite );
+ 		//window.draw( sprite );
+		window.draw(circle);
+		circle.move({5.f,0.f});
 
 
 		text.setString("Kjøøøøh!");
@@ -41,7 +73,7 @@ int main()
 
 
 
-		//Displayer nytt frame
+		//Render ny frame
 		window.display();
 	}
 }
