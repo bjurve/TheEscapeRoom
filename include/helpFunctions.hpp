6d90c1a17eff3069a079694b89c97b06fc15f50c
Sondre Bjerve <sondre00bjerve@gmail.com>
2026-09-03 15:40:32 +0200

    la til noen template-funksjoner og noen vanlige

--- include/helpFunctions.hpp
+++ include/helpFunctions.hpp
@@ -4,6 +4,20 @@
 #include "player.hpp"
 #include "button.hpp"
 #include "screenText.hpp"
+#include <cmath>
+#include <random>
+
+
+sf::Vector2f randomCoordinates(float maxX, float maxY);
+
+float randomCoordinateX(float maxX);
+float randomCoordinateY(float maxY);
+
+float randomNumber(float n);
+
+
+
+
 
 //Produserer sentrumspunktetkoordinater til diverse objekter
 sf::Vector2f getOriginCenterPlayer(const Player& p);
@@ -16,6 +30,40 @@ void mergeTextButton(screenText& text, Button button);
 
 //returnerer true hvis knapp er trykket
 bool buttonClicked(sf::Window& window, Button button);
+bool textClicked(sf::Window& window, screenText text);
+
+float randomPlusOrMinus(float n);
+
+sf::Vector2f randomFloats(float n);
+
+//----------------Templates -------------------
+
+//returnerer True hvis objekt a og b krysser hverandre
+template<typename T, typename U>
+bool objectIntersect(T a,U b)
+{
+
+	if(a.getGlobalBounds().findIntersection(b.getGlobalBounds()))
+	{
+		return true;
+	}
+
+	return false;
+};
+
+//returnerer True hvis objekt krysser spillvindu
+template<typename T>
+bool objectTouchWindowBorder(T& t)
+{
+    sf::FloatRect bounds = t.getGlobalBounds();		
+	if(bounds.position.x < 0){t.move({5.0f,0.0f}); return true;}                          
+	if(bounds.position.x + bounds.size.x > 1500.0f){t.move({-5.0f,0.0f}); return true;}
+	if(bounds.position.y < 0){t.move({0.0f,5.0f}); return true;}
+	if(bounds.position.y + bounds.size.y > 800.0f){t.move({0.0f,-5.0f}); return true;}
+	return false;
+}
+
+
 
 
 
--- src/helpFunctions.cpp
+++ src/helpFunctions.cpp
@@ -3,6 +3,50 @@
 #include "../include/helpFunctions.hpp"
 
 
+//returerer Random X og Y koordinater basert på 0 til max for x og y
+sf::Vector2f randomCoordinates(float maxX, float maxY)
+{
+	static std::random_device rd;
+	static std::mt19937 engine(rd());
+    
+	std::uniform_real_distribution<float> distX(0.0f, maxX - 200.0f);
+	std::uniform_real_distribution<float> distY(0.0f, maxY - 50.0f);
+	
+	return sf::Vector2f(distX(engine), distY(engine));
+}
+
+float randomCoordinateX(float maxX)
+{
+	static std::random_device rd;
+	static std::mt19937 engine(rd());
+    
+	std::uniform_real_distribution<float> distX(0.0f, maxX);
+	
+	return distX(engine);
+}
+
+float randomCoordinateY(float maxY)
+{
+	static std::random_device rd;
+	static std::mt19937 engine(rd());
+    
+	std::uniform_real_distribution<float> distX(0.0f, maxY);
+	
+	return distX(engine);
+}
+
+float randomNumber(float n)
+{
+    static std::random_device rd;
+	static std::mt19937 engine(rd());
+    
+	std::uniform_real_distribution<float> distX(0.0f,n);
+
+    return distX(engine);
+};
+
+
+
 
 
 //Produserer sentrumspunktkoordinater til objekt
@@ -14,7 +58,7 @@ sf::Vector2f getOriginCenterPlayer(const Player& p)
 
 sf::Vector2f getOriginCenterButton(const Button& b)
 {
-    sf::FloatRect bound = b.getButton().getGlobalBounds();
+    sf::FloatRect bound = b.getButtonConst().getGlobalBounds();
     return {bound.position.x + bound.size.x / 2 , bound.position.y + bound.size.y / 2};
 }
 
@@ -31,7 +75,7 @@ void mergeTextButton(screenText& text, Button button)
     text.getText().setPosition(button.getButton().getPosition());
 }
 
-//Returnerer True hvis knapp trykkes på
+//Returnerer True hvis knapp trykkes
 bool buttonClicked(sf::Window& window, Button button)
 {
     sf::Vector2i mousePos = sf::Mouse::getPosition(window);
@@ -45,3 +89,36 @@ bool buttonClicked(sf::Window& window, Button button)
 
     return false;
 }
+
+bool textClicked(sf::Window& window, screenText text)
+{
+    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+    sf::Vector2f mousdPosF(mousePos);
+    sf::Mouse::Button mousButton = sf::Mouse::Button::Left;    
+
+    if(text.getText().getGlobalBounds().contains(mousdPosF) && sf::Mouse::isButtonPressed(mousButton))
+    {
+	    return true;
+    }
+
+    return false;
+}
+
+float randomPlusOrMinus(float n)
+{
+    if(randomNumber(10) < 5){return n;}
+
+    return 0 - n;
+};
+
+sf::Vector2f randomFloats(float n)
+{
+	float x{randomPlusOrMinus(randomNumber(n))};
+	float y{randomPlusOrMinus(randomPlusOrMinus(n))};
+	return {x,y};
+};
+
+
+
+
+
